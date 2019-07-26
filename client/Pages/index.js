4f5868edd3fba3ab20b25e686ad2b9952f6274c2
yuchiu <yuchiu2002@hotmail.com>
2019-07-25 23:02:55 -0400

    added styling

--- client/Pages/index.js
+++ client/Pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { GlobalStyles } from "../Styles";
 
 const Home = () => (
   <React.Fragment>
     <Helmet>
       <title>Home Page</title>
     </Helmet>
+    <GlobalStyles />
     <div>aloha world</div>
   </React.Fragment>
 );
